fea6eb80314804e57d3c2efbb12f6b80887f1a25
Matthew <mattj20@gmail.com>
2022-08-15 12:39:54 -0700

    commit tree 1c1f7848fca4ea9f50983392244af5a9a906ebbb

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,21 @@
-def starts_with_a_vowel?(word)
+require 'pry'
 
+def starts_with_a_vowel?(word)
+    word.match?(/^[aeiou]|^[AEIOU]/)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+    text.scan(/un[a-z]*ing/)
 end
 
 def words_five_letters_long(text)
-
+    text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+    text.match?(/^[A-Z].*\W$/)
 end
 
 def valid_phone_number?(phone)
-
+    phone.match?(/(\D?)\b\d(\D?)\d(\D?)\d(\D?)\d(\D?)\d(\D?)\d(\D?)\d(\D?)\d(\D?)\d(\D?)\d\b(\D?)/)
 end
